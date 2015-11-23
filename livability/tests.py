2065069a0176e2033f89bb179d4604266bbe81eb
Dan Vigliotti <dan.vigliotti@webvig.com>
2015-11-23 13:13:42 -0600

    Updated model tests

--- livability/tests.py
+++ livability/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
+from models import *
+
 # Create your tests here.
+class ModelTest(TestCase):
+    def get_tool(self, name="Active Community Schools Workbook"):
+        return Tools.objects.get(name=name)
+
+    def test_tool_get(self):
+        tool = self.get_tool()
+        self.assertTrue(isinstance(tool, Tools))
+        self.asserEqual(tool.__unicode__(), tool.name)
