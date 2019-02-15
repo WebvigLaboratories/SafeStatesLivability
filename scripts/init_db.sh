b4ccca238f9b55e600c9189988d721e0d9399fbd
Dan Vigliotti <dan.vigliotti@webvig.com>
2019-02-15 15:23:15 -0500

    Initial setup to push livability to PCF

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,7 +3,7 @@ echo "------ Load database tables ------"
 python manage.py migrate --noinput
 
 echo "------ create default admin user ------"
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('dan.vigliotti', 'dan.vigliotti@webvig.com', 'e06B19c59')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('dan.vigliotti', 'dan.vigliotti@webvig.com', 'Changeme')" | python manage.py shell
 
 echo "------ Import initial data ------"
 python manage.py loaddata
