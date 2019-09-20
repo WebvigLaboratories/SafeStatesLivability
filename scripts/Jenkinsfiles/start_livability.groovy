31b1c84e0d5cc2f7cdbb0fc294a4f62c9002b677
Dan Vigliotti <dan.vigliotti@travelport.com>
2019-09-20 08:29:10 -0400

    Added full pathing

--- scripts/Jenkinsfiles/start_livability.groovy
+++ scripts/Jenkinsfiles/start_livability.groovy
@@ -5,12 +5,20 @@ pipeline {
         }
     }
 
+    environment {
+        ANSIBLE_PLAYBOOK_CMD = "/Library/Frameworks/Python.framework/Versions/3.7/bin/ansible-playbook"
+        ANSIBLE_VAULT_PASSWORD_FILE = "/User/Shared/Jenkins"
+        PLAYBOOK_DIR = "scripts/ansible-playbooks"
+    }
+
     stages {
         stage('Ansible Playbook - Start livability app') {
             steps {
                 script {
                     echo "Ansible Playbook - Start livability app"
-                    sh "ansible-playbook -i scripts/ansible-playbooks/inventory.yml scripts/ansible-playbooks/start_livability.yml"
+                    sh "${ANSIBLE_PLAYBOOK_CMD} -i ${PLAYBOOK_DIR}/inventory.yml " +
+                            "${PLAYBOOK_DIR}/start_livability.yml " +
+                            "--vault-password=${ANSIBLE_VAULT_PASSWORD_FILE}"
                 }
             }
             post {
